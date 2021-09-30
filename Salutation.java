f81d48d15f46d7ef65ce4a58d05e309d85fad278
WooHyeon <91645912+muh0108@users.noreply.github.com>
2021-09-30 14:08:34 +0900

    Update Salutation.java

--- Salutation.java
+++ Salutation.java
@@ -2,22 +2,22 @@ import java.util.Calendar;
 
 public class Salutation {
 	public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        int Hour = cal.get(Calendar.HOUR_OF_DAY);
-        int Minute = cal.get(Calendar.MINUTE);
-        System.out.println("ÇöÀç ½Ã°£Àº " + Hour + "½Ã " + Minute + "ºÐÀÔ´Ï´Ù.");
- 
-        if (4 <= Hour && Hour < 12) {   
-            System.out.println("Good Morning");
-            //4:00 = Hour(4), 11:59 ¡Á Hour(12)
-        } else if (12 <= Hour && Hour < 18) { 
-            System.out.println("Good Afternoon");
-            // 12:00 = Hour(12) , 17:59 ¡Á Hour(18)
-        } else if (18 <= Hour && Hour < 22) {
-            System.out.println("Good Evening");
-            // 18:00 = Hour(18) , 21:59 ¡Á Hour(22)
-        }else {
-        	System.out.println("Good Night");
-        }
+		Calendar cal = Calendar.getInstance();
+		int Hour = cal.get(Calendar.HOUR_OF_DAY);
+		int Minute = cal.get(Calendar.MINUTE);
+		System.out.println("í˜„ìž¬ ì‹œê°„ì€ " + Hour + "ì‹œ " + Minute + "ë¶„ìž…ë‹ˆë‹¤.");
+
+		if (4 <= Hour && Hour < 12) {   
+		    System.out.println("Good Morning");
+		    //4:00 = Hour(4), 11:59 â‰  Hour(12)
+		} else if (12 <= Hour && Hour < 18) { 
+		    System.out.println("Good Afternoon");
+		    // 12:00 = Hour(12) , 17:59 â‰  Hour(18)
+		} else if (18 <= Hour && Hour < 22) {
+		    System.out.println("Good Evening");
+		    // 18:00 = Hour(18) , 21:59 â‰  Hour(22)
+		}else {
+			System.out.println("Good Night");
+		}
 	}
 }
